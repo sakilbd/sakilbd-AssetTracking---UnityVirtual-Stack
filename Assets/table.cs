3624df0cca5c2234d1a90ce27e6d965b26c6de3f
Md Sakiluzzaman <55885272+sakilbd@users.noreply.github.com>
2021-03-02 18:49:39 +0600

    3d text rack name from json Done

--- Assets/table.cs
+++ Assets/table.cs
@@ -3,12 +3,12 @@ using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System.IO;
-
+[System.Serializable]
  public class table : MonoBehaviour {
 
 
 
-
+   public TMPro.TMP_Text rackNme;
      public GameObject block1;
 
        public GameObject boxes;
@@ -32,10 +32,35 @@ private float count;
     }
 
     void CreateWorld (string jsonD) {
+ PlayerInfo[] PInfo = JsonHelper.getJsonArray<PlayerInfo>(jsonD);
+
+//  var fib = new List<int>{0,1,2};
+// foreach( object elem in PInfo){
+//  string s = PInfo[elem].rack_name;
+//         Debug.Log(s);
+
+// }
 
          count=1f;
          worldHeight = rackNumber/2-1 ;
 
+          for (int i = 0; i >= 0; i++)
+          {
+            try
+                                        {
+                                        
+        string s = PInfo[i].rack_name;
+        rackNme.text= s;
+        Debug.Log(s);
+
+                                        }
+ catch (System.Exception excp)
+                    {
+
+//                        Debug.Log("sexy");
+                        break;
+                    }
+          }
          for(float x = 0f; x < worldHeight; x++) {
             //    Debug.Log( "firstLoop:"+x);
             for (float z = 0f; z < worldWidth; z++) {
@@ -45,6 +70,11 @@ private float count;
                     GameObject block = Instantiate(block1, Vector3.zero , block1.transform.rotation) as GameObject;
                     block.transform.parent = transform;
                     block.transform.localPosition = new Vector3( z + z * gap ,0, x + x * gap);
+
+                    // string s = PInfo[i].rack_name;
+        // rackNme.text= s;
+        // Debug.Log(s);
+
                     // Debug.Log(block);
                     count++;
                     
@@ -77,7 +107,34 @@ private float count;
     //        }
     //    }
     //}
+[System.Serializable]
+public class PlayerInfo
+{
+    // public string id;
+    // public string product_name;
+    // public string order_id;
+    // public string cell_position;
+    // public string rf_card_number;
+    // public string destination;
+     public string rack_name;
 
+}
+[System.Serializable]
+public class JsonHelper
+{
+    public static T[] getJsonArray<T>(string json)
+    {
+        string newJson = "{ \"array\": " + json + "}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.array;
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
 
 
 
